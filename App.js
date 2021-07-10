9739b320d743e4cf8700baff41b62763a1c57f6b
yoongyum <72365815+yoongyum@users.noreply.github.com>
2021-07-10 23:08:26 +0900

    add image upload

--- App.js
+++ App.js
@@ -1,29 +1,29 @@
 import React, { useState } from "react";
-import { useFonts } from "expo-font";
 import AppLoading from "expo-app-loading";
 import { Asset } from "expo-asset";
 import Navi from "./components/Navi";
 
 export default class App extends React.Component {
-   state = {
-      isReady: false,
-   };
+    state = {
+        isReady: false,
+    };
 
-   render() {
-      if (!this.state.isReady) {
-         return <AppLoading startAsync={this._cacheResourcesAsync} onFinish={() => this.setState({ isReady: true })} onError={console.warn} />;
-      } else {
-         return <Navi />;
-      }
-   }
+    render() {
+        if (!this.state.isReady && !this.state.fontsLoaded) {
+            return <AppLoading startAsync={this._cacheResourcesAsync} onFinish={() => this.setState({ isReady: true })} onError={console.warn} />;
+        } else {
+            return <Navi />;
+        }
+    }
 
-   //이미지 불러오기
-   async _cacheResourcesAsync() {
-      const images = [require("./assets/images/EyeOff.png"), require("./assets/images/EyeOn.png")];
+    //이미지 불러오기
+    async _cacheResourcesAsync() {
+        const images = [require("./assets/images/EyeOff.png"), require("./assets/images/EyeOn.png"), require("./assets/images/Camera.png"), require("./assets/images/Pick.png")];
 
-      const cacheImages = images.map((image) => {
-         return Asset.fromModule(image).downloadAsync();
-      });
-      return Promise.all(cacheImages);
-   }
+        const cacheImages = images.map(image => {
+            return Asset.fromModule(image).downloadAsync();
+        });
+
+        return Promise.all(cacheImages);
+    }
 }
